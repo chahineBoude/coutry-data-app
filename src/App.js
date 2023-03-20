c2a9a887bd476d2443e11ae4bbdef390ba2052af
chahineBoude <chahineboude@gmail.com>
2023-03-20 15:41:30 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,22 +7,14 @@ import Modal from "./component/Modal";
 const App = () => {
   const [openPopup, setOpenPopup] = useState(true);
 
-  /*   let hasPopupShown = localStorage.getItem("hasPopupShown");
-
-
-  if (!hasPopupShown || hasPopupShown === null) {
-    setOpenPopup(true);
-    localStorage.setItem("hasPopupShown", true);
-  } */
-
-  const handleClose = () => {
+  /*  const handleClose = () => {
     setOpenPopup(false);
     localStorage.setItem("item", "popup");
   };
 
   if (localStorage.getItem("item") !== "popup") {
     return <Modal open={openPopup} handleClose={handleClose} />;
-  }
+  } */
 
   return (
     <>
