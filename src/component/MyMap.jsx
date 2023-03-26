2f15e6758a1771e2ff55cdbbd02eff823efa684d
chahineBoude <chahineboude@gmail.com>
2023-03-26 14:30:16 +0100

    Update MyMap.jsx

--- src/component/MyMap.jsx
+++ src/component/MyMap.jsx
@@ -16,11 +16,11 @@ function MyMap() {
 
   function formatNumber(num) {
     if (num >= 1000000000) {
-      return (num / 1000000000).toFixed(3) + "B";
+      return (num / 1000000000).toFixed(2) + "B";
     } else if (num >= 1000000) {
-      return (num / 1000000).toFixed(3) + "M";
+      return (num / 1000000).toFixed(2) + "M";
     } else if (num >= 1000) {
-      return (num / 1000).toFixed(3) + "k";
+      return (num / 1000).toFixed(2) + "k";
     } else {
       return num;
     }
